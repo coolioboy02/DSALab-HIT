08d7c3ee8b935777eb1ee09ae5386f3499d1eedc
biocool2 <135319735+coolioboy02@users.noreply.github.com>
2026-06-10 10:26:05 +0700

    Update and rename giai week6 to giai week6.cpp

---
+++ exercises/week06/giai week6.cpp
@@ -0,0 +1,19 @@
+//Cau 1
+//them dau
+void themdau(list&1,Node* p){
+    if (1.pHead == NULL){
+        1.pHead = 1.pTail =p;
+    } else{
+            p->pNext = 1.pHead;
+            1.pHead = p;
+    }
+}
+//them cuoi
+void themcuoi(List&1, Node* p){
+    if (1.pHead == NULL){
+        1.pHead = 1.pTail =p;
+    } else{
+            1.pTail->pNext = p;
+            1.pTail->p;
+    }
+}
